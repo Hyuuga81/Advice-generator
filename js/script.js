19b35a5fd1ed612a8621523de6d8dbfb5f0d5b0b
Jay <j.adeyina@gmail.com>
2024-03-07 13:06:50 +0900

    Mobile styles

--- js/script.js
+++ js/script.js
@@ -1,22 +1,29 @@
 const adviceNo = document.querySelector('.advice-no');
 const advice = document.querySelector('.advice');
-const choice = Math.floor(Math.random() * 223);
+const dice = document.querySelector('.dice');
 
 
-const adviceGetter = async function() {
-    let res = await fetch(`https://api.adviceslip.com/advice/${choice}`);
-    let data = await res.json();
-    console.log(data);
 
-    let dataAdvice = data.slip.advice;
-    let dataAdviceNo = data.slip.id;
 
-    displayAdvice(dataAdvice, dataAdviceNo)
-};
+dice.addEventListener('click', function() {
+    const choice = Math.floor(Math.random() * 223);
+    const adviceGetter = async function() {
+        let res = await fetch(`https://api.adviceslip.com/advice/${choice}`);
+        let data = await res.json();
+        console.log(data);
 
+        let dataAdvice = data.slip.advice;
+        let dataAdviceNo = data.slip.id;
+
+        displayAdvice(dataAdvice, dataAdviceNo)
+    };   
 adviceGetter();
+});
+
+
 
 const displayAdvice = function(dataAdvice, dataAdviceNo) {
-        adviceNo.innerText = dataAdviceNo;
-        advice.innerText = dataAdvice;
+        adviceNo.innerText = `Advice #${dataAdviceNo}`;
+        advice.innerText = `"${dataAdvice}"`;
 };
+
